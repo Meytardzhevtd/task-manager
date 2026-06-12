49cd3a189b0cdeb97ab6d54d182c6da86d840c28
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-06-13 00:38:52 +0300

    add Create method

--- internal/repository/UserRepo.go
+++ internal/repository/UserRepo.go
@@ -1,7 +1,20 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"task-manager/internal/model"
+)
 
 type UserRepository struct {
 	db *sql.DB
+}
+
+func (r *UserRepository) Create(username, email, password string) (*model.User, error) {
+	query := `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`
+	user := &model.User{Username: username, Email: email, Password: password}
+	err := r.db.QueryRow(query, username, email, password).Scan(&user.ID, &user.CreateAt)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
